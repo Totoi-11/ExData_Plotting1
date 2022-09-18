8e8e2db33652a404dc133c739d81f206077aebda
Totoi-11 <108863651+Totoi-11@users.noreply.github.com>
2022-09-18 21:23:01 +0800

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,11 @@
+> setwd("E:/Coursera/Data Science/Exploratory Data Analysis")
+> dir()
+[1]"household_power_consumption.txt"     
+> files <- file("household_power_consumption.txt")
+> household <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value = TRUE),sep = ';',col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"), na.strings = '?')
+> household$Date <- as.Date(household$Date, format = '%d/%m/%Y')
+> household$DateTime <- as.POSIXct(paste(household$Date,household$Time))
+> Sys.setlocale(category = "LC_ALL", locale = "english")
+> plot(household$DateTime,household$Global_active_power,xlab = "",ylab = "Global Active Power (kilowatts)", type = "l")
+> dev.copy(png, file="plot2.png")
+> dev.off()
